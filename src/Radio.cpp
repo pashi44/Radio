31f1f324942cffa92c14e6ae66bb814e6ad02cf5
pashi44 <pashireddi@gmail.com>
2025-11-20 22:17:53 -0600

    nothing works

--- src/Radio.cpp
+++ src/Radio.cpp
@@ -4,11 +4,10 @@
 #include <zephyr/drivers/uart.h>
 #include <zephyr/logging/log.h>
 
-
+#ifdef CONFIG_RADIO_UART_INSTANCE
 #include "uart.hpp"
+#endif
 
-// #ifdef CONFIG_RADIO_UART_INSTANCE
-// #endif   //UART
 
 
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
@@ -18,16 +17,48 @@ LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
 int main(void)
 {
-    
+    const struct device *uart_dev1 = DEVICE_DT_GET(UART1_NODE);
+    const struct device *uart_dev2 = DEVICE_DT_GET(UART2_NODE);
 
-    
+    if (!device_is_ready(uart_dev1)) {
+        LOG_ERR("UART1 (serial1 alias) not ready");
+        return -ENODEV;
+    } else {
+        LOG_INF("UART1 device is ready");
+    }
 
-    while(1){k_msleep(5);}
+    if (!device_is_ready(uart_dev2)) {
+        LOG_ERR("UART2 (uart2 node) not ready");
+        return -ENODEV;
+    } else {
+        LOG_INF("UART2 device is ready");
+    }
+
+    LOG_INF("Starting UART TX on UART1 and RX on UART2");
+    unsigned char tx[] =   {'p', 'r','a', 's', 'h','i', '\n'};
+
+    // unsigned char* tx  = "prashanth";
+size_t len = sizeof(tx) / sizeof(tx[0]);
+
+    while (true) {
 
 
+for (size_t i = 0; i < len; i++) {
+        uart_poll_out(uart_dev1, tx[i]);
+        k_msleep(50);
+    }
 
 
+        unsigned char rx;
+        int ret = uart_poll_in(uart_dev2, &rx);
+        if (ret == 0) {
+            printk("UART2 RX: '%c' (0x%02x)\n",
+                   (rx >= 32 && rx <= 126) ? rx : '.',
+                   rx);
+        }
 
+        k_msleep(5);
+    }
 
     return 0;
 }

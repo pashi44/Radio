c94b91173cc98a8c9d07c5adea3cb279842de4f7
pashi44 <pashireddi@gmail.com>
2025-11-21 20:56:22 -0600

    m

--- drivers/uart/include/uart.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include <zephyr/kernel.h>
-
-// Put any small helper inline functions or types here if needed.
---
+++ drivers/uartone/include/uart.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <zephyr/drivers/uart.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(uartone, CONFIG_LOG_DEFAULT_LEVEL);
